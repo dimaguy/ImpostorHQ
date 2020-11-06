5c53f418a5babd708281ab58a07403be92e9f295
dimaguy <afonsoantonio2003@gmail.com>
2020-11-06 21:01:33 +0000

    Delete cp.bat

--- cp.bat
+++
@@ -1,3 +0,0 @@
-rd /s/q C:\Users\fnafm\source\repos\ImpostorHQ\Server.Test\dashboard\
-mkdir C:\Users\fnafm\source\repos\ImpostorHQ\Server.Test\dashboard\
-xcopy /s ImpostorHQ.Dashboard Server.Test\dashboard /e /v
